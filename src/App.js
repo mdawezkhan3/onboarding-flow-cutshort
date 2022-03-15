7fdf8459319f51edb82548ac1f966d0a2aa2bfa5
Awez Khan <mdawezkhan3@gmail.com>
2022-03-15 21:31:01 +0530

    reduced top padding

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className='flex flex-col items-center justify-center mt-24'>
+    <div className='flex flex-col items-center justify-center mt-12'>
       <div className='flex justify-center items-center mb-12'>
         <img src='/logo.png' alt="Return on Ad Spend" className='pb-1' />
         <p className='text-4xl font-bold pl-2'>Eden</p>
